ea25c07fc6705951e95cb4c83b78786d15565026
panc86 <panc86@gmail.com>
2021-04-05 15:45:12 +0200

    simplify passenger_title function

--- features.py
+++ features.py
@@ -18,13 +18,15 @@ def passenger_name_size(df):
 # TITLE
 def passenger_title(df):
     # title mapping
-    title_map = {'ms': 'mrs', 'mme': 'mrs', 'mlle': 'miss'}
-    title_map.update({title: 'rare' for title in ['dr', 'rev', 'sir', 'don', 'jonkheer', 'lady', 'the countess', 'col', 'major', 'capt', 'dona']})
+    titles_map = {
+        'ms': 'mrs', 'mme': 'mrs', 'mlle': 'miss', # normalize
+        'mr': 'mr', 'master': 'master', 'miss': 'miss', 'mrs': 'mrs' # defaults
+    }
     # the Title of the passenger indicates gender, age, and social class
     def parse_title(name):
         # title is between last and first names
         title = name.split(',')[1].lstrip().split('.')[0].lower()
-        return title_map.get(title, title)
+        return titles_map.get(title, "rare")
     # passenger's title
     return df["Name"].apply(parse_title).astype("category")
 
